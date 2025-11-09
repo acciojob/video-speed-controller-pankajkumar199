083dfad34915657706ee29519e68d7ab42cdd49b
pankajkumar199 <pk228624@gmail.com>
2025-11-09 23:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('[name="volume"]');
+const playbackSpeed = document.querySelector('[name="playbackSpeed"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function handleVolumeUpdate() {
+  video.volume = this.value;
+}
+
+function handleSpeedUpdate() {
+  video.playbackRate = this.value;
+}
+
+function skip() {
+  video.currentTime += Number(this.dataset.skip);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.width = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+volume.addEventListener('input', handleVolumeUpdate);
+playbackSpeed.addEventListener('input', handleSpeedUpdate);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+progress.addEventListener('click', scrub);
